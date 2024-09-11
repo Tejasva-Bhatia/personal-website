7bda014ead4da65632d992d179ffe677af3a2cbd
Tejasva-Bhatia <bhatiatejasva15@gmail.com>
2024-09-11 01:45:51 -0500

    stats page updated

--- src/data/stats/personal.js
+++ src/data/stats/personal.js
@@ -5,7 +5,7 @@ const Age = () => {
 
   const tick = () => {
     const divisor = 1000 * 60 * 60 * 24 * 365.2421897; // ms in an average year
-    const birthTime = new Date('1990-02-05T09:24:00');
+    const birthTime = new Date('1997-07-15T06:45:00');
     setAge(((Date.now() - birthTime) / divisor).toFixed(11));
   };
 
@@ -27,14 +27,39 @@ const data = [
   {
     key: 'countries',
     label: 'Countries visited',
-    value: 53,
+    value: 5,
     link:
-      'https://www.google.com/maps/d/embed?mid=1iBBTscqateQ93pWFVfHCUZXoDu8&z=2',
+      'https://www.google.com/maps/d/u/0/viewer?mid=1Ye2RwCuPAfQdugQGNwXuZI48JjpB51k&ll=20.5429490427026%2C166.30805450000003&z=3',
   },
   {
     key: 'location',
     label: 'Current city',
-    value: 'New York, NY',
+    value: 'Winnipeg, MB ❄️',
+  },
+  {
+    key: 'Favorite movie',
+    label: 'Favorite movie',
+    value: 'Interstellar 🎬',
+  },
+  {
+    key: 'Favorite TV show',
+    label: 'Favorite TV show',
+    value: 'Breaking Bad 🧪',
+  },
+  {
+    key: 'Favorite Author right now',
+    label: 'Favorite Author right now',
+    value: 'Robert Greene 📖',
+  },
+  {
+    key: 'Go-to sport to play right now',
+    label: 'Go-to sport to play right now',
+    value: 'Badminton 🏸',
+  },
+  {
+    key: 'Preferred coding language',
+    label: 'Preferred coding language',
+    value: 'C++ 💻',
   },
 ];
 
